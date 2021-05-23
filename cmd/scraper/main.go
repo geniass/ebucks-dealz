01d5b75f79df3a3d511dd2fc7d683634b6644b19
Ari Croock <acroock@isaziconsulting.co.za>
2021-05-23 22:40:35 +0200

    refactor to run as github action

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
+	"flag"
 	"log"
 	"os"
 	"path/filepath"
@@ -16,77 +16,56 @@ var safeFilenameReplaceRegex = regexp.MustCompile(`[^a-zA-Z0-9-]+`)
 
 func main() {
 
-	// c := colly.NewCollector(
-	// 	colly.AllowedDomains("www.ebucks.com"),
-	// 	colly.URLFilters(
-	// 		regexp.MustCompile(`https://www\.ebucks\.com/web/shop/shopHome\.do`),
-	// 		regexp.MustCompile(`https://www\.ebucks\.com/web/shop/categorySelected\.do.*`),
-	// 		regexp.MustCompile(`https://www\.ebucks\.com/web/shop/productSelected\.do.*`),
-	// 	),
-	// 	colly.CacheDir("./cache"),
-	// )
+	dirNameArg := flag.String("dir", "./data", "directory in which to write scraped data files")
+	overwriteArg := flag.Bool("overwrite", false, "when false, a new directory is created within the data dir named as the current date and time; otherwise the data dir is cleaned and replaced.")
 
-	// c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-	// 	link := e.Attr("href")
-	// 	c.Visit(e.Request.AbsoluteURL(link))
-	// })
+	flag.Parse()
 
-	// runDate := time.Now()
-	// c.OnHTML(".product-detail-frame", func(e *colly.HTMLElement) {
-	// 	p := Product{
-	// 		URL:        e.Request.URL.String(),
-	// 		Name:       e.ChildText("h2.product-name"),
-	// 		Price:      e.ChildText("#randPrice"),
-	// 		Savings:    e.ChildText(".was-price > strong:nth-child(1) > span:nth-child(1)"),
-	// 		Percentage: e.ChildText("table#discount-table tr:last-child td.discount-icon p.percentage"),
-	// 		RunDate:    runDate,
-	// 	}
+	dirname := *dirNameArg
 
-	// 	dirname := filepath.Join("data", runDate.Format("2006-01-02T15-04-05Z-0700"))
-	// 	if err := os.MkdirAll(dirname, os.ModeDir|0755); err != nil {
-	// 		log.Fatal(err)
-	// 	}
+	if !*overwriteArg {
+		runDate := time.Now()
+		dirname = filepath.Join(dirname, runDate.Format("2006-01-02T15-04-05Z-0700"))
+	}
 
-	// 	name := safeFilenameReplaceRegex.ReplaceAllString(p.Name, "-")
-	// 	fmt.Println(name)
-	// 	f, err := os.Create(filepath.Join(dirname, name+".json"))
-	// 	if err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// 	defer f.Close()
+	if err := os.MkdirAll(dirname, os.ModeDir|0755); err != nil {
+		log.Fatal(err)
+	}
 
-	// 	encoder := json.NewEncoder(f)
-	// 	if err := encoder.Encode(p); err != nil {
-	// 		log.Fatal(err)
-	// 	}
-	// })
-
-	// c.OnRequest(func(r *colly.Request) {
-	// 	fmt.Println("Visiting", r.URL.String())
-	// })
-
-	// if err := c.Visit("https://www.ebucks.com/web/shop/shopHome.do"); err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	runDate := time.Now()
 	s := scraper.NewScraper(func(p scraper.Product) {
-		dirname := filepath.Join("data", runDate.Format("2006-01-02T15-04-05Z-0700"))
-		if err := os.MkdirAll(dirname, os.ModeDir|0755); err != nil {
-			log.Fatal(err)
-		}
-
 		name := safeFilenameReplaceRegex.ReplaceAllString(p.Name, "-")
-		fmt.Println(name)
-		f, err := os.Create(filepath.Join(dirname, name+".json"))
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer f.Close()
 
-		encoder := json.NewEncoder(f)
-		if err := encoder.Encode(p); err != nil {
-			log.Fatal(err)
+		func() {
+			f, err := os.Create(filepath.Join(dirname, name+".json"))
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer f.Close()
+
+			encoder := json.NewEncoder(f)
+			if err := encoder.Encode(p); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		if p.Percentage != "" {
+			func() {
+				percentDirname := filepath.Join(dirname, p.Percentage)
+				if err := os.MkdirAll(percentDirname, os.ModeDir|0755); err != nil {
+					log.Fatal(err)
+				}
+
+				f, err := os.Create(filepath.Join(percentDirname, name+".json"))
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer f.Close()
+
+				encoder := json.NewEncoder(f)
+				if err := encoder.Encode(p); err != nil {
+					log.Fatal(err)
+				}
+			}()
 		}
 	})
 
